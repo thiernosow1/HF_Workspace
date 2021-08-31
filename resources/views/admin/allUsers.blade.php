f477cba282a5270b10de2f6e0965bdb73edd2531
thiernosow1 <boubasow660@gmail.com>
2021-09-01 00:53:50 +0200

    style modification

--- resources/views/admin/allUsers.blade.php
+++ resources/views/admin/allUsers.blade.php
@@ -5,8 +5,12 @@
 <section>
     {{-- Section contenant toute la liste de sites --}}
     @foreach ($users as $user )
-    <p> {{$user->email}}</p>
-    <a href={{"/admin/users/" . $user->id . "/delete"}}> <button> Supprimer </button></a>
+    <div class="users">
+        <p>Nom : {{$user->name}}  {{$user->firstname}}</p>
+        <p> Email :  {{$user->email}}</p>
+        <p>Inscrit depuis {{$user->created_at}}</p>
+        <a href={{"/admin/users/" . $user->id . "/delete"}}> <button class="buttonSupprimer"> Supprimer </button></a>
+    </div>
 
     @endforeach
 
